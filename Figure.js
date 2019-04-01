6adb2df7fbd2b78c3b28cca5fa56e6bc7562ee0a
Pozelaite <jana.pozelaite@accenture.com>
2019-04-01 12:02:41 +0300

    Refactored code in the master branch

--- Figure.js
+++ Figure.js
@@ -7,48 +7,32 @@ class Figure{
 
   moveFigure(spaces) {
     
-    
-    let spacess = spaces;
     const prom = new Promise((resolve)=> {
       let el = document.getElementById(this.id);
       let offset = 110;
       
       let s = setInterval(()=>{
-        let space = spacess.find((s)=>{
-          return s.y === el.y.animVal.value && s.x === el.x.animVal.value
+
+        let nextspace = spaces.find((s)=>{
+          return s.y === el.y.animVal.value+30 && s.x === el.x.animVal.value
         })
-        console.log(space.isAvailable, space.id)
-        if (offset<600&&space.isAvailable===true){
-            let nextspace = spacess.find((s)=>{
-              return s.y === el.y.animVal.value+30 && s.x === el.x.animVal.value
-            })
-            if(nextspace.isAvailable){
-              offset = offset + 30;
-              el.setAttribute('y', offset-30);
-            }else {
-              spacess.forEach((sp)=>{
-                if(sp.y === el.y.animVal.value && sp.x === el.x.animVal.value){
-                  sp.isAvailable = false
-                }
-              })
-              clearInterval(s);
-              resolve(spacess);
-            }
-            
-         
-         
+
+        if (offset<600&&nextspace.isAvailable===true){
+      
+          offset = offset + 30;
+          el.setAttribute('y', offset-30);
+
         } else {
-          space.isAvailable === false
-          space = null
+
           offset = 80;
-          
-          spacess.forEach((sp)=>{
+
+          spaces.forEach((sp)=>{
             if(sp.y === el.y.animVal.value && sp.x === el.x.animVal.value){
               sp.isAvailable = false
             }
           })
           clearInterval(s);
-          resolve(spacess);
+          resolve(spaces);
         }
       },500)
     });
